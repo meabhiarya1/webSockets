a3ac9a6fb86352007b689f5cee1a230af5bacde9
Abhishek kumar <abhi.arya97161@gmail.com>
2025-02-14 12:38:37 +0530

    Add initial server setup with WebSocket support and package configuration

---
+++ Server/index.js
@@ -0,0 +1,18 @@
+import express from "express"; // Import express
+import { WebSocketServer } from "ws";
+
+const app = express(); // Create express app
+
+const port = 8080; // Port number
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+const wss = new WebSocketServer({ server });
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {
+    console.log("%s: ", data);
+    ws.send("Hello from server ");
+  });
+});
